274891a6b76f75d8337e9f3d7ba769567721fb71
Jurij <plaskan.jurij@gmail.com>
2023-09-30 14:00:27 +0200

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,12 @@
+function changeForm(){
+    document.getElementById("registracijaStranke").style.display="block";
+    document.getElementById("prijavaStranke").style.display="none";
+}
+function changeForm2(){
+    document.getElementById("registracijaStranke").style.display="none";
+    document.getElementById("prijavaStranke").style.display="block";
+}
+
+function vpisUporabnika(){
+    return;
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mysql2 = require('mysql2');
+const port = process.env.PORT || 3000;
+const hostname = 'localhost';
+
+const app=express();
+
+app.use(express.static(__dirname+'/src'));
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/public/index.html');
+});
+
+app.listen(port);
+console.log('Server on http://localhost:'+port);
